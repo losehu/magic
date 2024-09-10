e7d34eecd54082fbe6b943cb4812cd2b56ee08d8
losehu <wu58430@126.com>
2024-09-10 09:21:37 +0800

    fid完整计算

--- tools/fid_all.py
+++ tools/fid_all.py
@@ -4,10 +4,16 @@ import shutil
 import os
 import re
 import matplotlib.pyplot as plt
-
+from PIL import Image
 weight_paths=[
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E34-S20575",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E35-S21150",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E36-S21725",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E37-S22300",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E38-S22875",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E39-S23450",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E40-S24025",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E41-S24600",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E42-S25175",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E43-S25750",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E44-S26325",
@@ -19,7 +25,10 @@ weight_paths=[
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E50-S29775",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E51-S30350",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E52-S30925",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E53-S31500",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E54-S32075",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E55-S32650",
+"/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E58-S34375",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E60-S35525",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E64-S37825",
 "/root/autodl-tmp/magic-main/magicdrive-log/SDv1.5mv-rawbox_2024-08-27_23-10_224x400/weight-E68-S40125",
@@ -46,25 +55,28 @@ def read_data(file_path):
     return x, y
 
 
-# 绘制并保存图形
 def plot_data(x, y, output_file):
+    # 对 x 和 y 数据根据 x 的值进行排序
+    sorted_data = sorted(zip(x, y), key=lambda pair: pair[0])
+    x_sorted, y_sorted = zip(*sorted_data)  # 解压成两个列表
+
     plt.figure(figsize=(12, 8))
-    plt.plot(x, y, marker='o', linestyle='-', color='b', label='Data')
-    plt.xlabel('X (First Column)')
-    plt.ylabel('Y (Second Column)')
+    plt.plot(x_sorted, y_sorted, marker='o', linestyle='-', color='b', label='Data')
+    plt.xlabel('STEP')
+    plt.ylabel('FID')
     plt.title('FID')
     plt.legend()
     plt.grid(True)
 
     # 计算x轴的范围
-    x_min, x_max = min(x), max(x)
+    x_min, x_max = min(x_sorted), max(x_sorted)
     # 扩展x轴范围以减少标签重叠
     x_padding = (x_max - x_min) * 0.1
     plt.xlim(x_min - x_padding, x_max + x_padding)
 
-
     plt.savefig(output_file)  # 保存图形到文件
     plt.close()  # 关闭图形，以便释放资源
+
 
 def launch_accelerate():
     for weight_path in weight_paths:
@@ -98,10 +110,41 @@ def launch_accelerate():
             'fid.rootb='+tmp_dir
         ]
         subprocess.run(command2, check=True)
-        file_path = '/root/autodl-tmp/magic-main/fid.txt'  # 替换为您的文件路径
-        output_file = '/root/autodl-tmp/magic-main/fid.png'  # 设置保存图形的文件名
-        x, y = read_data(file_path)
-        plot_data(x, y, output_file)
+    #绘制FID图像
+    file_path = 'fid.txt'  # 替换为您的文件路径
+    output_file = 'fid.png'  # 设置保存图形的文件名
+    x, y = read_data(file_path)
+    plot_data(x, y, output_file)
+    #将图片统一输出
+    img_names=[
+    "1531281439800013_gen_0.jpg",
+    "1538985054947711_gen_0.jpg",
+    "1542799485148043_gen_0.jpg",
+    ]
+    if not os.path.exists('./tmp_sum'):
+        os.makedirs('./tmp_sum')
+    for img_name in img_names:
+        directory_path='./tmp_sum/'+str(re.match(r'^(\d+)_gen_', img_name).group(1))
+        if not os.path.exists(directory_path):
+            os.makedirs(directory_path)
+        for weight_path in weight_paths:
+            match = str(re.search(r'(\d+)$', weight_path).group(1))
+            img_dir='tmp-'+match+"/224x400/samples"+"/CAM_PANO/"+img_name
+            if os.path.isfile(img_dir):
+                # if not os.path.isfile(directory_path+"/"+img_name)
+                shutil.copy2(img_dir, directory_path+"/"+match+'.png')
+                input_image_path = img_dir 
+                output_image_path = directory_path+"/"+match+'.png' 
+                with Image.open(input_image_path) as img:
+                    img_resized = img.resize((2400, 160) , Image.BICUBIC)
+                    img_resized.save(output_image_path)
+            else:
+                print(f'{img_dir} not exist')
+
+
+
+
+
 
 if __name__ == "__main__":
     launch_accelerate()
